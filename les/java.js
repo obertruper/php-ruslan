91141f7f23c1aef767db08afa3a2dc9522750873
AntonLapin <anton11844@gmail.com>
2018-07-16 20:32:29 +0300

    first html-page

--- les/java.js
+++ les/java.js
@@ -1,25 +1,7 @@
-function cube(num) {
-    return num * num * num ;
-}
+var user = {};
 
-function min(num1, num2) {
-    if (num1 < num2) {
-        return num1
-    } else {
-        return num2
-    }
- }
+user.name = 'Вася';
+user.surname= 'петров';
+user.name ='Сергей';
 
- function dayOfWeek(day) {
-    switch ((day)) {
-        case 1: return 'пн';
-        case 2: return 'вт';
-        case 3: return 'ср';
-        case 4: return 'чт';
-        case 5: return 'пт';
-        case 6: return 'сб';
-        case 7: return 'вс';
-    }
-     return 'неверный день'
- }
-  var result
+delete user.name ;
